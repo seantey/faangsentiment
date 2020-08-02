90aafd17c4eab0e2ae0026338885347fc65ac1d9
Sean Tey <stey@dons.usfca.edu>
2020-08-01 22:03:58 -0700

    update to match new script name

--- scripts/emr/gen_test_results.sh
+++ scripts/emr/gen_test_results.sh
@@ -5,10 +5,10 @@
 # on my system at the time which is about 7GB.
 # The spark-submit version ran fast and smoothly using only 2GB~3GB max.
 
-spark-submit spark_script.py '2020-07-19_Hour=15'
-spark-submit spark_script.py '2020-07-20_Hour=15'
-spark-submit spark_script.py '2020-07-21_Hour=15'
-spark-submit spark_script.py '2020-07-22_Hour=15'
-spark-submit spark_script.py '2020-07-23_Hour=15'
-spark-submit spark_script.py '2020-07-24_Hour=15'
-spark-submit spark_script.py '2020-07-25_Hour=15'
+spark-submit spark_process_sentiment.py '2020-07-19_Hour=15'
+spark-submit spark_process_sentiment.py '2020-07-20_Hour=15'
+spark-submit spark_process_sentiment.py '2020-07-21_Hour=15'
+spark-submit spark_process_sentiment.py '2020-07-22_Hour=15'
+spark-submit spark_process_sentiment.py '2020-07-23_Hour=15'
+spark-submit spark_process_sentiment.py '2020-07-24_Hour=15'
+spark-submit spark_process_sentiment.py '2020-07-25_Hour=15'
